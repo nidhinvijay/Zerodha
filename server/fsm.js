7a48eefc2e3d24e4503d94946f6a7801e9b29a02
nidhinvijay <nidhinvijaynv@gmail.com>
2026-01-01 14:19:50 +0530

    Fix duplicate SELL log

--- server/fsm.js
+++ server/fsm.js
@@ -56,14 +56,13 @@ class FSM {
       }
     } else if (signal.intent === 'SELL') {
       const prevState = this.state;
-      if (this.state === STATES.BUYPOSITION) {
-        this.logStateChange('SELL_SIGNAL_EXIT', { prevState, exitPrice: this.ltp });
-      }
+      const wasInPosition = this.state === STATES.BUYPOSITION;
       this.state = STATES.NOSIGNAL;
       this.threshold = null;
       this.blockedAtMs = null;
       this.entryPrice = null;
-      this.logStateChange('SELL_SIGNAL', { prevState });
+      // Log only once
+      this.logStateChange(wasInPosition ? 'SELL_EXIT' : 'SELL_SIGNAL', { prevState, exitPrice: wasInPosition ? this.ltp : null });
     }
     return this.getSnapshot();
   }
